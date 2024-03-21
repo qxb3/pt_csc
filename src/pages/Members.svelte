76a19ae3d99d484a3c7bfcd486b50c115b4cd52d
qxb3 <qxbthree@gmail.com>
2024-03-21 22:50:52 +0800

    final memebers update

--- src/pages/Members.svelte
+++ src/pages/Members.svelte
@@ -2,40 +2,16 @@
   <h1 class="title">Members</h1>
 
   <div class="members">
-    <div class="member">
-      <p class="name">Franc Sant Alcaide</p>
-      <p class="quote">- Planado kona.</p>
-    </div>
-  </div>
-
-  <div class="member">
+    <p class="name">Franc Sant Alcaide</p>
     <p class="name">John Lou Wyn Aparis</p>
-    <p class="quote">- King Louis.</p>
-  </div>
-
-  <div class="member">
+    <p class="name">Yahj Galleto</p>
     <p class="name">Angelo Gabriel Panes</p>
-    <p class="quote">- Ang ingay mo nuevo.</p>
-  </div>
-
-  <div class="member">
+    <p class="name">Mary Faye Paraiso</p>
     <p class="name">Adam Nuevo III</p>
-    <p class="quote">- brawdjawj.</p>
-  </div>
-
-  <div class="member">
     <p class="name">Genrev Abraham Ordonio</p>
-    <p class="quote">- Walang kwenta si pres amp.</p>
-  </div>
-
-  <div class="member">
-    <p class="name">Justin Karl Salimbagat (Salimbat, Salibingot, Salimburat)</p>
-    <p class="quote">- Na code kona yan par.</p>
-  </div>
-
-  <div class="member">
-    <p class="name">Lord Vincent Torio (Sapilot)</p>
-    <p class="quote">- Pahingi 2nd chance.</p>
+    <p class="name">Justin Karl Salimbagat</p>
+    <p class="name">Hanna Sabudin</p>
+    <p class="name">Lord Vincent Torio</p>
   </div>
 </main>
 
@@ -56,14 +32,8 @@
     margin-top: 2rem;
   }
 
-  .member .name {
+  .name {
     font-size: 14px;
     margin-bottom: 0;
-  }
-
-  .member .quote {
-    font-size: 14px;
-    font-style: italic;
-    margin-top: 0;
   }
 </style>
